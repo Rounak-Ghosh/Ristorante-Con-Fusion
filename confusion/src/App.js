ee608d45bd00dc40458bfd702213295a1a4797c7
Rounak-Ghosh <rounakghosh189@gmail.com>
2022-06-30 13:45:13 +0530

    Initial Commit

---
+++ confusion/src/App.js
@@ -0,0 +1,20 @@
+import Main from './components/MainComponent';
+import './App.css';
+import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Main/>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+  
+
+export default App;
---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+    function RenderDish({dish}) {
+        if(dish != null) {
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    function RenderComments({comments}) {
+        if(comments != null) {
+            const comm = comments.map(comment => {
+                return (
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author},
+                        &nbsp;
+                        {comment.date}
+                        </p>
+                    </li>
+                );
+            })
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <h4>Comments</h4>
+                    <ul className='list-unstyled'>
+                        {comm}
+                    </ul>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    const DishDetail = (props) => {
+        console.log(props.comments);
+        if(props.dish != null) {
+            return(
+                <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                    </div>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
+                    </div>
+                </div>
+            );
+        }        
+    }
+
+
+export default DishDetail;
